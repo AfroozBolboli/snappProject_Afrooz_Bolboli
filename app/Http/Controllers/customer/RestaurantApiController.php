36a743e05683a9aeb4eb87b48115b7c87eef7d62
Afrooz <shigurehyung@gmail.com>
2022-12-02 05:00:11 +0330

    feat: findNearestRestaurants in Customer show restaurants was added.

--- app/Http/Controllers/customer/RestaurantApiController.php
+++ app/Http/Controllers/customer/RestaurantApiController.php
@@ -6,17 +6,24 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\FoodCategoryResource;
 use App\Http\Resources\RestaurantResource;
 use App\Http\Resources\ShowFoodResource;
+use App\Models\Customer;
+use App\Models\CustomerAddress;
 use App\Models\Food;
 use App\Models\FoodCategory;
 use App\Models\Restaurant;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class RestaurantApiController extends Controller
 {
 
     public function index()
     {
-        return RestaurantResource::collection(Restaurant::all());
+        $userCurrentAddress = json_decode(Customer::find(auth()->user()->id)->currentAddress);
+
+        $restaurants = self::findNearestRestaurants($userCurrentAddress->latitude, $userCurrentAddress->longitude, 1000);
+
+        return RestaurantResource::collection($restaurants);
     }
 
     public function show($id)
@@ -28,16 +35,33 @@ class RestaurantApiController extends Controller
     {
         $categories = Food::select('category')->distinct()->where('restaurant_id', $id)->get();
         $categoriesArray = [];
-        for($i=0 ; $i < count($categories); $i++)
-        {
+        for ($i = 0; $i < count($categories); $i++) {
             $categoriesArray[] = FoodCategory::where('title', $categories[$i]->category)->get();
             $categoriesArray[$i] = $categoriesArray[$i][0];
         }
 
         return ['categories' => FoodCategoryResource::collection($categoriesArray)];
-
     }
 
+    private function findNearestRestaurants($latitude, $longitude, $radius = 400)
+    {
+        $restaurants = Restaurant::selectRaw("id, name, address, categories, status, restaurantPicture, latitude, longitude,
+                         ( 6371 * acos( cos( radians(?) ) *
+                           cos( radians( latitude ) )
+                           * cos( radians( longitude ) - radians(?)
+                           ) + sin( radians(?) ) *
+                           sin( radians( latitude ) ) )
+                         ) AS distance", [$latitude, $longitude, $latitude])
+            ->where('status', '=', 1)
+            ->having("distance", "<", $radius)
+            ->orderBy("distance", 'asc')
+            ->offset(0)
+            ->limit(20)
+            ->get();
+
+        return $restaurants;
+    }
+    
     public function isOpen()
     {
         return ['Restaurants' => RestaurantResource::collection(Restaurant::where('status', 1)->get())];
@@ -52,5 +76,4 @@ class RestaurantApiController extends Controller
     {
         return ['Restaurants' => RestaurantResource::collection(Restaurant::where('score', $value)->get())];
     }
-
 }
